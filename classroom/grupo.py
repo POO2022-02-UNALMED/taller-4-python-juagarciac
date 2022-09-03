43e888c15d36537341767a106520482c72d4d890
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-09-02 21:16:41 -0500

    j

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,37 +1,44 @@
-
-
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
-
 
 class Grupo:
 
-    grado = None
+    __grado="Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
-
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
-
-    def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
-            self.listadoAlumnos = [alumno]
+    def __str__(self):
+        return "Grupo de estudiantes:"+self._grupo
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def grado(cls):
+        return cls.__grado
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
+        cls.__grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    def listadoAsignaturas(self, **kwargs):
+        if (self._asignaturas==None):
+            p=[]
+            for x in kwargs.values():
+                p.append(kwargs[x])
+            self._asignaturas=p
+        else:
+            for x in kwargs.values():
+                self._asignaturas.append(kwargs[x])
+
+    def agregarAlumno(self, alumno, lista=None):
+        if(self.listadoAlumnos==None):
+            if (lista!=None):
+                lista.append(alumno)
+                self.listadoAlumnos =lista
+            else:
+                self.listadoAlumnos=alumno
+        else:
+            if (lista!=None):
+                lista.append(alumno)
+                self.listadoAlumnos =self.listadoAlumnos+lista
+            else:
+                self.listadoAlumnos=self.listadoAlumnos+alumno
